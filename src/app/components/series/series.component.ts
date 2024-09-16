c1acc210d2c98890ce981cda33d6623bb0b99465
Your Name <you@example.com>
2024-09-16 22:38:36 +0300

    add components and routes, finished sign-in component

---
+++ src/app/components/series/series.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-series',
+  standalone: true,
+  imports: [],
+  templateUrl: './series.component.html',
+  styles: ``
+})
+export class SeriesComponent {
+
+}
---
+++ src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [FormsModule,
+    CommonModule
+  ],
+  templateUrl: './sign-in.component.html',
+  styles: ``
+})
+export class SignInComponent {
+  username:string = "";
+  password:string = "";
+
+  user:string = "george";
+  pass:string = "1234";
+
+  constructor(private _router:Router){}
+
+  Login(){
+    alert(`Hello ${this.username}`);
+    this._router.navigate(["/home"]);
+  }
+
+}
